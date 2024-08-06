90ed1b5c42d5a1f512a2f71cda8a28f3af8c18ea
kp <krittayos.kp@gmail.com>
2024-08-06 14:54:18 +0700

    update: tailwind config color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,31 +24,8 @@ module.exports = {
             desktop: { min: '1024px' },
          },
          colors: {
-            orange: '#FF712B',
-            yellow: '#FF9E00',
-            blue: '#2B99FF',
-            'light-blue': { 100: '#4977D6', 200: '#E8EFFC' },
-            'dark-blue': {
-               100: '#012060',
-               200: '#2C3975',
-            },
-            purple: {
-               100: '#573c82',
-            },
-            green: '#34B53A',
-            red: '#FF3A29',
-            pink: '#E90E8B',
-            black: {
-               100: '#383A48',
-               200: '#7D7D7D',
-            },
-            gray: {
-               100: '#BCBEC0',
-               200: '#E5E5E5',
-               300: '#B4AAA6',
-            },
-            'menu-text': '#7F828C',
-            'menu-color': '#131420',
+            primary: '#012060',
+            secondary: '#2C3975',
          },
       },
    },
