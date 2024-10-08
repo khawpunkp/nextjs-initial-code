c75c254b118103579b45d21d1bb32281d25b5b16
kp <krittayos.kp@gmail.com>
2024-10-08 13:47:27 +0700

    update: loading component

--- src/components/elementLoading.tsx
+++ src/components/elementLoading.tsx
@@ -1,8 +1,12 @@
 import { CircularProgress } from '@mui/material'
 
-export default function ElementLoading() {
+type Props = {
+   isLoading?: boolean
+}
+
+export default function ElementLoading({ isLoading = true }: Props) {
    return (
-      <div className='w-full h-full max-h-[calc(100vh-64px)] flex justify-center items-center bg-white/10 absolute top-0 left-0 backdrop-blur-sm z-50'>
+      <div className={`w-full h-full max-h-[calc(100vh-64px)] flex justify-center items-center bg-white/10 absolute top-0 left-0 backdrop-blur-sm z-50 ${isLoading ? '' : 'hidden'}`}>
          <CircularProgress />
       </div>
    )
--- src/components/pageLoading.tsx
+++ src/components/pageLoading.tsx
@@ -2,12 +2,13 @@ import { CircularProgress, Typography } from '@mui/material'
 
 type Props = {
    label?: JSX.Element | string
+   isLoading?: boolean
 }
 
-export default function PageLoading({ label }: Props) {  
+export default function PageLoading({ label, isLoading = true }: Props) {
    return (
-      <div>
-         <div className='fixed inset-0 flex flex-col gap-6 items-center justify-center z-[99] w-full h-full bg-white text-purple-100'>
+      <div className={isLoading ? '' : 'hidden'}>
+         <div className='fixed inset-0 flex flex-col gap-6 items-center justify-center z-[99] w-full h-full bg-white text-primary'>
             <Typography variant='h6'>{label}</Typography>
             <CircularProgress />
          </div>
